99020bbcc83af769ce328f59b4ba4de49062ecdb
Stephen Eley <sfeley@gmail.com>
2011-07-15 13:31:40 -0400

    Enable local .chef directory for testing

--- features/support/env.rb
+++ features/support/env.rb
@@ -17,9 +17,12 @@ World do
 end
 
 Around('@invalid_credentials') do |scenario, block|
+  # Move current working directory if one exists (and restore at end)
+  FileUtils.mv(".chef", ".chef_cucumber_temp") if File.exist?(".chef")
   FileUtils.mkdir_p(".chef")
   config = "chef_node_name 'REALLYBOGUSORGNAME'"
   File.open(".chef/knife.rb", 'w') { |f| f.puts config }
   block.call
   FileUtils.rm_rf(".chef")
+  FileUtils.mv(".chef_cucumber_temp", ".chef") if File.exist?(".chef_cucumber_temp")
 end
