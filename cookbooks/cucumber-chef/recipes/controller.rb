d60da3998572e26f375679b2ccecf76ed9db16d5
Zachary Patten <zim931@gmail.com>
2012-05-24 17:35:44 -0700

    constant rename

--- cookbooks/cucumber-chef/recipes/controller.rb
+++ cookbooks/cucumber-chef/recipes/controller.rb
@@ -1,6 +1,5 @@
-LXC_ROOTFS = "/var/lib/lxc/controller/rootfs"
+ROOTFS = "/var/lib/lxc/controller/rootfs"
 controllers = search(:node, 'name:cucumber-chef-controller')
-status = %x(lxc-info -n controller 2>&1)
 
 # create the network configuration for our 'controller' lxc container
 cookbook_file "/etc/lxc/controller" do
@@ -14,12 +13,12 @@ execute "lxc-create -n controller -f /etc/lxc/controller -t lucid-chef" do
 end
 
 # execute our chef installation script in the 'controller' lxc container
-execute "chroot #{LXC_ROOTFS} /bin/bash -c '/tmp/install-chef.sh'" do
+execute "chroot #{ROOTFS} /bin/bash -c '/tmp/install-chef.sh'" do
   not_if { controllers.length > 0 }
 end
 
 # install our chef 'client.rb' into our 'controller' lxc container
-template "#{LXC_ROOTFS}/etc/chef/client.rb" do
+template "#{ROOTFS}/etc/chef/client.rb" do
   source "controller-client.erb"
   variables(:orgname => node["cucumber-chef"]["orgname"],
             :nodename => node["cucumber-chef"]["nodename"])
@@ -27,17 +26,18 @@ template "#{LXC_ROOTFS}/etc/chef/client.rb" do
 end
 
 # install our chef "bootstrap" json data
-cookbook_file "#{LXC_ROOTFS}/etc/chef/first-boot.json" do
+cookbook_file "#{ROOTFS}/etc/chef/first-boot.json" do
   source "controller-first-boot"
   not_if { controllers.length > 0 }
 end
 
 # execute our chef "bootstrap"
-execute "chroot #{LXC_ROOTFS} /bin/bash -c '/usr/bin/chef-client -j /etc/chef/first-boot.json'" do
+execute "chroot #{ROOTFS} /bin/bash -c '/usr/bin/chef-client -j /etc/chef/first-boot.json'" do
   not_if { controllers.length > 0 }
 end
 
 # fire up the 'controller' lxc container
+status = %x(lxc-info -n controller 2>&1)
 execute "lxc-start -d -n controller" do
   not_if { status.include?("RUNNING") }
 end
