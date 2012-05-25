82252f0f3500069992eb78c8f84e9d18e2ba4512
Zachary Patten <zim931@gmail.com>
2012-05-24 17:35:35 -0700

    updated to use opscode omnibus

--- cookbooks/cucumber-chef/recipes/test_lab.rb
+++ cookbooks/cucumber-chef/recipes/test_lab.rb
@@ -1,16 +1,41 @@
-%w(rsync build-essential libxml2-dev libxslt1-dev).each do |p|
+%w(build-essential wget ruby-full ruby-dev libxml2-dev libxslt1-dev).each do |p|
   package p
 end
 
-%w(rspec cucumber cucumber-chef).each do |g|
-  gem_package g
+bash "install rubygems" do
+  code <<-EOH
+cd /tmp
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.8.19.tgz
+tar zxf rubygems-1.8.19.tgz
+cd rubygems-1.8.19
+ruby setup.rb --no-format-executable
+  EOH
 end
 
-directory "/root/.ssh" do
-  owner "root"
-  mode "0600"
+%w(rspec cucumber-chef).each do |g|
+  gem_package g do
+    gem_binary("/usr/bin/gem")
+  end
 end
 
-cookbook_file "/root/.ssh/config" do
-  source "ssh-config"
+%w(root ubuntu).each do |user|
+  directory "/#{user}/.ssh" do
+    owner user
+    group user
+    mode "0700"
+  end
+
+  cookbook_file "/#{user}/.ssh/config" do
+    source "ssh-config"
+    owner user
+    group user
+    mode "0600"
+  end
+
+  cookbook_file "/#{user}/.gemrc" do
+    source "gem-rc"
+    owner user
+    group user
+    mode "0644"
+  end
 end
