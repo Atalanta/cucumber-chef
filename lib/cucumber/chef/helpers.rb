c7345d0d7b8747bd742a6175ada4318fe068fb3c
Zachary Patten <zim931@gmail.com>
2012-05-28 14:26:08 -0700

    add extended output to command runner exceptions

--- lib/cucumber/chef/helpers.rb
+++ lib/cucumber/chef/helpers.rb
@@ -6,19 +6,19 @@ module Cucumber
 
       def run_chroot_command(name, command)
         output = %x(chroot #{lxc_rootfs(name)} /bin/bash -c '#{command}' 2>&1)
-        raise "run_chroot_command failed" if ($? != 0)
+        raise "run_chroot_command(#{command}) failed (#{$?})" if ($? != 0)
         output
       end
 
       def run_remote_command(name, command)
         output = %x(ssh #{@servers[name][:ip]} '#{command}' 2>&1)
-        raise "run_remote_command failed" if ($? != 0)
+        raise "run_remote_command(#{command}) failed (#{$?})" if ($? != 0)
         output
       end
 
       def run_command(command)
         output = %x(#{command})
-        raise "run_command failed" if ($? != 0)
+        raise "run_command(#{command}) failed (#{$?})" if ($? != 0)
         output
       end
 
