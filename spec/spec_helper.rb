6db74831728cbbcee80b0067c8523741d115a732
Zachary Patten <zim931@gmail.com>
2012-05-27 20:44:50 -0700

    removed requires; unused method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,31 +1,9 @@
-require "rubygems"
-require "bundler/setup"
 require File.join(File.dirname(__FILE__), "../lib/cucumber-chef")
-require "simplecov"
 
 SimpleCov.start do
   add_filter '/spec/'
-
 #  add_group 'cucumber-chef', '/lib/'
 end if ENV["COVERAGE"]
-
-def tcp_test_ssh(hostname)
-  tcp_socket = TCPSocket.new(hostname, 22)
-  IO.select([tcp_socket], nil, nil, 5)
-rescue Errno::ETIMEDOUT
-  false
-rescue Errno::EPERM
-  false
-rescue Errno::ECONNREFUSED
-  sleep 2
-  false
-  # This happens on EC2 quite often
-rescue Errno::EHOSTUNREACH
-  sleep 2
-  false
-ensure
-  tcp_socket && tcp_socket.close
-end
 
 %w{ORGNAME OPSCODE_USER}.each do |var|
   if ENV[var].nil? || ENV[var].empty?
