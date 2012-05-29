2b8e4e404a8738c013508829c501568f6820d46f
Zachary Patten <zim931@gmail.com>
2012-05-29 16:53:21 -0700

    add STDOUT, STDERR, STDIN to initializer and pass to SSH class initialize

--- lib/cucumber/chef/test_runner.rb
+++ lib/cucumber/chef/test_runner.rb
@@ -6,13 +6,15 @@ module Cucumber
 
       require 'cucumber/chef/test_lab'
 
-      def initialize(config, project_dir)
+      def initialize(config, project_dir, stdout=STDOUT, stderr=STDERR, stdin=STDIN)
         @config = config
         @project_dir = project_dir
+        @stdout, @stderr, @stdin = stdout, stderr, stdin
+        @stdout.sync = true
 
         @test_lab = Cucumber::Chef::TestLab.new(@config)
 
-        @ssh = Cucumber::Chef::SSH.new
+        @ssh = Cucumber::Chef::SSH.new(@stdout, @stderr, @stdin)
         @ssh.config[:hostname] = @test_lab.labs_running.first.public_ip_address
         @ssh.config[:ssh_user] = "ubuntu"
         @ssh.config[:identity_file] = File.expand_path(@config[:knife][:identity_file])
