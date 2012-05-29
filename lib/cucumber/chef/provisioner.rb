8d331b84d0f5a3a641f4ce62759c36bf2d4538a9
Zachary Patten <zim931@gmail.com>
2012-05-29 16:19:29 -0700

    rewrote provisioner class to use internal bootstrapping class along with changes to support chef-server on test-lab

--- lib/cucumber/chef/provisioner.rb
+++ lib/cucumber/chef/provisioner.rb
@@ -2,83 +2,86 @@ require "digest"
 
 module Cucumber
   module Chef
-    class ProvisionerError < Error ; end
+    class ProvisionerError < Error; end
 
     class Provisioner
       attr_accessor :stdout, :stderr, :stdin
 
-      def initialize(config, stdout=STDOUT, stderr=STDERR, stdin=STDIN)
-        @stdout, @stderr, @stdin = stdout, stderr, stdin
+      def initialize(config, server, stdout=STDOUT, stderr=STDERR, stdin=STDIN)
         @config = config
-        @cookbook_path = File.join(File.dirname(__FILE__), "../../../cookbooks/cucumber-chef")
+        @server = server
+        @stdout, @stderr, @stdin = stdout, stderr, stdin
+        @stdout.sync = true
+
+        @cookbooks_path = Pathname.new(File.join(File.dirname(__FILE__), "../../../cookbooks/"))
+        @roles_path = Pathname.new(File.join(File.dirname(__FILE__), "../../../roles/"))
       end
 
-      def bootstrap_node(server)
-        template_file = File.join(File.dirname(__FILE__), "bootstrap/ubuntu-#{@config[:knife][:ubuntu_release]}.erb")
-        @stdout.puts("Using bootstrap template '#{template_file}'.")
-        run_bootstrap(template_file, server, chef_node_name, "role[test_lab]")
+      def build
+        template_file = File.join(File.dirname(__FILE__), "../../../lib/cucumber/chef/bootstrap/ubuntu-chef-server.erb")
+        template_file = Pathname.new(template_file).expand_path
+
+        bootstrap(template_file)
+        download_credentials
+        upload_cookbook
+        upload_role
         tag_node
-      end
-
-      def upload_cookbook
-        version_loader = ::Chef::Cookbook::CookbookVersionLoader.new(@cookbook_path)
-        version_loader.load_cookbooks
-        uploader = ::Chef::CookbookUploader.new(version_loader.cookbook_version, @cookbook_path)
-        uploader.validate_cookbook
-        uploader.upload_cookbook
-      end
-
-      def upload_role
-        role_path = File.join(@cookbook_path, "roles")
-        ::Chef::Config[:role_path] = role_path
-        role = ::Chef::Role.from_disk("test_lab")
-        role.save
-      end
-
-      def tag_node
-        node = ::Chef::Node.load(chef_node_name)
-        node.tags << (@config.test_mode? ? :test : :user)
-        node.save
       end
 
 
     private
 
-      def run_bootstrap(template_file, server, node_name, run_list=nil)
-        @stdout.puts("Preparing bootstrap for '#{server.public_ip_address}'.")
-
-        bootstrap = ::Chef::Knife::Bootstrap.new
-        ui = ::Chef::Knife::UI.new(@stdout, @stderr, @stdin, bootstrap.config)
-        bootstrap.ui = ui
-        bootstrap.name_args = [server.public_ip_address]
-        bootstrap.config[:run_list] = run_list
-        bootstrap.config[:ssh_user] = "ubuntu"
-        bootstrap.config[:ssh_password] = "ubuntu"
-        bootstrap.config[:identity_file] = @config[:knife][:identity_file]
-        bootstrap.config[:chef_node_name] = node_name
-        bootstrap.config[:use_sudo] = true
-        bootstrap.config[:template_file] = template_file
-        bootstrap.config[:validation_client_name] = @config["validation_client_name"]
-        bootstrap.config[:validation_key] = @config["validation_key"]
-        bootstrap.config[:chef_server_url] = @config["chef_server_url"]
-        bootstrap.config[:distro] = "ubuntu-#{@config[:knife][:ubuntu_release]}"
-        bootstrap.config[:host_key_verify] = false
-
-        @stdout.puts("Running bootstrap for '#{server.public_ip_address}'.")
-        bootstrap.run
-
-        @stdout.puts("Finished bootstrapping '#{server.public_ip_address}'.")
-        bootstrap
+      def run_command(command)
+        command = "#{command} 2>&1"
+        @stdout.puts("run_command(#{command})")
+        @stdout.puts(%x(#{command}))
+        raise ProvisionerError, "run_command(#{command}) failed! (#{$?})" if ($? != 0)
       end
 
-      def chef_node_name
-        @node_name ||= begin
-          if @config.test_mode?
-            "cucumber-chef-#{Digest::SHA1.hexdigest(Time.now.to_s)[0..7]}"
-          else
-            "cucumber-chef-test-lab"
-          end
+      def knife_command(*args)
+        "knife #{args.join(" ")} -u ubuntu -k .cucumber-chef/ubuntu.pem -s http://#{@server.public_ip_address}:4000 --color -n"
+      end
+
+      def bootstrap(template_file)
+        bootstrap = Cucumber::Chef::Bootstrap.new
+        bootstrap.config[:host] = @server.public_ip_address
+        bootstrap.config[:ssh_user] = "ubuntu"
+        bootstrap.config[:use_sudo] = true
+        bootstrap.config[:identity_file] = @config[:knife][:identity_file]
+        bootstrap.config[:template_file] = template_file
+        bootstrap.config[:context][:hostname] = "cucumber-chef-test-lab"
+        bootstrap.config[:context][:chef_server] = @server.public_ip_address
+        bootstrap.config[:context][:amqp_password] = "p@ssw0rd1"
+        bootstrap.config[:context][:admin_password] = "p@ssw0rd1"
+        bootstrap.run
+      end
+
+      def download_credentials
+        ssh = Cucumber::Chef::SSH.new
+        ssh.config[:host] = @server.public_ip_address
+        ssh.config[:ssh_user] = "ubuntu"
+        ssh.config[:identity_file] = @config[:knife][:identity_file]
+        local_path = File.join(Dir.pwd, ".cucumber-chef")
+        remote_path = "/home/#{ssh.config[:ssh_user]}/.chef"
+
+        FileUtils.mkdir_p(local_path)
+
+        files = [ "#{ssh.config[:ssh_user]}.pem" ]
+        files.each do |file|
+          ssh.download(File.join(remote_path, file), File.join(local_path, file))
         end
+      end
+
+      def upload_cookbook
+        run_command(knife_command("cookbook upload cucumber-chef", "-o", @cookbooks_path.expand_path))
+      end
+
+      def upload_role
+        run_command(knife_command("role from file", @roles_path.join("test_lab.rb").expand_path))
+      end
+
+      def tag_node
+        run_command(knife_command("tag create cucumber-chef-test-lab", @config.mode))
       end
 
     end
