c9b26f257ff20495dbca6919791431bdd1028a2e
Zachary Patten <zim931@gmail.com>
2012-05-31 20:51:50 -0700

    properly include helpers

--- lib/cucumber/chef/helpers.rb
+++ lib/cucumber/chef/helpers.rb
@@ -1,6 +1,23 @@
-include Cucumber::Chef::Helpers::Chef
-include Cucumber::Chef::Helpers::Command
-include Cucumber::Chef::Helpers::LXC
-include Cucumber::Chef::Helpers::Server
-include Cucumber::Chef::Helpers::TestLab
-include Cucumber::Chef::Helpers::Utility
+module Cucumber
+  module Chef
+    module Helpers
+
+      require 'cucumber/chef/helpers/chef'
+      require 'cucumber/chef/helpers/command'
+      require 'cucumber/chef/helpers/lxc'
+      require 'cucumber/chef/helpers/server'
+      require 'cucumber/chef/helpers/test_lab'
+      require 'cucumber/chef/helpers/utility'
+
+      def self.included(base)
+        base.send(:include, Cucumber::Chef::Helpers::Chef)
+        base.send(:include, Cucumber::Chef::Helpers::Command)
+        base.send(:include, Cucumber::Chef::Helpers::LXC)
+        base.send(:include, Cucumber::Chef::Helpers::Server)
+        base.send(:include, Cucumber::Chef::Helpers::TestLab)
+        base.send(:include, Cucumber::Chef::Helpers::Utility)
+      end
+
+    end
+  end
+end
