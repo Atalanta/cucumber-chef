4c4311ab035b5bb6a255f842bac2a467279c71fa
Zachary Patten <zim931@gmail.com>
2012-06-03 03:52:20 -0700

    removed un-needed dhcpd directive; switched dhcp3-server restart to use init.d instead of service

--- lib/cucumber/chef/helpers/test_lab.rb
+++ lib/cucumber/chef/helpers/test_lab.rb
@@ -1,20 +1,19 @@
 module Cucumber::Chef::Helpers::TestLab
 
   def test_lab_config_dhcpd
-    dhcpd_lxc_config = File.join("/etc/dhcp3/lxc.conf")
-    File.open(dhcpd_lxc_config, 'w') do |f|
-      f.puts(Cucumber::Chef.generate_do_not_edit_warning("LXC DHCPD Configuration"))
+    dhcpd_config = File.join("/etc/dhcp3/test-lab.conf")
+    File.open(dhcpd_config, 'w') do |f|
+      f.puts(Cucumber::Chef.generate_do_not_edit_warning("DHCPD Configuration"))
       $servers.each do |key, value|
-        f.puts("")
+        f.puts
         f.puts("host #{key} {")
         f.puts("  hardware ethernet #{value[:mac]};")
         f.puts("  fixed-address #{value[:ip]};")
-        f.puts("  option host-name \"#{key}\";")
         f.puts("  ddns-hostname \"#{key}\";")
         f.puts("}")
       end
     end
-    command_run_local("service dhcp3-server restart")
+    command_run_local("/etc/init.d/dhcp3-server restart")
   end
 
 end
