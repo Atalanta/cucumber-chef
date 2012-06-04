c5295d1b683fcf8ddd29dbc03581c3b308fd4736
Zachary Patten <zim931@gmail.com>
2012-06-03 23:36:57 -0700

    added logging statements

--- lib/cucumber/chef/bootstrap.rb
+++ lib/cucumber/chef/bootstrap.rb
@@ -6,6 +6,8 @@ module Cucumber
     class Bootstrap
       attr_accessor :stdout, :stderr, :stdin, :config
 
+################################################################################
+
       def initialize(stdout=STDOUT, stderr=STDERR, stdin=STDIN)
         @stdout, @stderr, @stdin = stdout, stderr, stdin
         @stdout.sync = true if @stdout.respond_to?(:sync=)
@@ -15,27 +17,53 @@ module Cucumber
         @config[:context] = Hash.new(nil)
       end
 
-      def run
-        raise BootstrapError, "You must supply a 'template_file' option." if !@config[:template_file]
-        raise BootstrapError, "You must supply a 'host' option." if !@config[:host]
-        raise BootstrapError, "You must supply a 'ssh_user' option." if !@config[:ssh_user]
-        raise BootstrapError, "You must supply a 'ssh_password' or 'identity_file' option." if (!@config[:ssh_password] && !@config[:identity_file])
+################################################################################
 
-        $logger.debug { "Preparing bootstrap for '#{@config[:host]}'." }
+      def run
+        $logger.debug { "config(#{@config.inspect})" }
+
+        if !@config[:template_file]
+          message = "You must supply a 'template_file' option."
+          $logger.fatal { message }
+          raise BootstrapError, message
+        end
+
+        if !@config[:host]
+          message = "You must supply a 'host' option."
+          $logger.fatal { message }
+          raise BootstrapError, message
+        end
+
+        if !@config[:ssh_user]
+          message = "You must supply a 'ssh_user' option."
+          $logger.fatal { message }
+          raise BootstrapError, message
+        end
+
+        if (!@config[:ssh_password] && !@config[:identity_file])
+          message = "You must supply a 'ssh_password' or 'identity_file' option."
+          $logger.fatal { message }
+          raise BootstrapError, message
+        end
+
+        $logger.debug { "prepare(#{@config[:host]})" }
+
         @ssh.config[:host] = @config[:host]
         @ssh.config[:ssh_user] = @config[:ssh_user]
         @ssh.config[:ssh_password] = @config[:ssh_password]
         @ssh.config[:identity_file] = @config[:identity_file]
         @ssh.config[:timeout] = 5
 
-        $logger.debug { "Using template_file '#{@config[:template_file]}'." }
+        $logger.debug { "template_file(#{@config[:template_file]})" }
         command = Cucumber::Chef::Template.render(@config[:template_file], @config[:context])
         command = "sudo #{command}" if @config[:use_sudo]
 
-        $logger.debug { "Running bootstrap for '#{@config[:host]}'." }
+        $logger.debug { "begin(#{@config[:host]})" }
         @ssh.exec(command, :silence => true)
-        $logger.debug { "Finished bootstrap for '#{@config[:host]}'." }
+        $logger.debug { "end(#{@config[:host]})" }
       end
+
+################################################################################
 
     end
 
