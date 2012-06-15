d5241b976ac9d41cee2d6344bdb46803e6c4e4bb
Zachary Patten <zim931@gmail.com>
2012-06-15 14:09:45 -0500

    OCD

--- lib/cucumber/chef/helpers/chef_server.rb
+++ lib/cucumber/chef/helpers/chef_server.rb
@@ -24,13 +24,13 @@ module Cucumber::Chef::Helpers::ChefServer
 ################################################################################
 
   def chef_server_node_destroy(name)
-    Chef::Node.load(name).destroy rescue nil
+    (Chef::Node.load(name).destroy rescue nil)
   end
 
 ################################################################################
 
   def chef_server_client_destroy(name)
-    Chef::ApiClient.load(name).destroy rescue nil
+    (Chef::ApiClient.load(name).destroy rescue nil)
   end
 
 ################################################################################
