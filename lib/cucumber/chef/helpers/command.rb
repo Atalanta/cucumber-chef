35d060c561f95dcba8deb5adecbd0134757139b0
zpatten <zpatten@jovelabs.com>
2013-01-15 16:00:55 -0800

    removed old commented out code; wrap commands with 'bash -c'

--- lib/cucumber/chef/helpers/command.rb
+++ lib/cucumber/chef/helpers/command.rb
@@ -24,14 +24,9 @@ module Cucumber::Chef::Helpers::Command
 ################################################################################
 
   def command_run_remote(name, command, expected_exit_code=0)
-    output = %x(ssh #{name} '#{command}' 2>&1)
+    output = %x(ssh #{name} /bin/bash -c '#{command}' 2>&1)
     raise "command_run_remote(#{command}) failed (#{$?})" if ($? != expected_exit_code)
     output
-#  rescue RuntimeError => e
-#    if $? == 65280
-#      puts "Exit Code #{$?}: Retrying..."
-#      retry
-#    end
   end
 
 ################################################################################
@@ -45,7 +40,7 @@ module Cucumber::Chef::Helpers::Command
 ################################################################################
 
   def command_run_local(command, expected_exit_code=0)
-    output = %x(#{command} 2>&1)
+    output = %x(/bin/bash -c '#{command}' 2>&1)
     raise "command_run_local(#{command}) failed (#{$?})" if ($? != expected_exit_code)
     output
   end
