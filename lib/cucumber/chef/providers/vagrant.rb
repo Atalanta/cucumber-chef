36c40f0880edfd04b3b67f7d0014ca91b4f35889
zpatten <zpatten@jovelabs.com>
2013-01-22 02:23:49 -0800

    implement vagrant

--- lib/cucumber/chef/providers/vagrant.rb
+++ lib/cucumber/chef/providers/vagrant.rb
@@ -25,61 +25,102 @@ module Cucumber
       class VagrantError < Error; end
 
       class Vagrant
-        attr_accessor :stdout, :stderr, :stdin, :logger
+        attr_accessor :vagrant, :stdout, :stderr, :stdin, :logger
 
 ################################################################################
 
         def initialize(stdout=STDOUT, stderr=STDERR, stdin=STDIN, logger=$logger)
           @stdout, @stderr, @stdin, @logger = stdout, stderr, stdin, logger
           @stdout.sync = true if @stdout.respond_to?(:sync=)
+
+          @vagrant = ::Vagrant::Environment.new
         end
 
 ################################################################################
 
         def create
-          raise "Not Implemented!"
+          @vagrant.cli("up")
 
           self
         end
 
         def destroy
-          raise "Not Implemented!"
+          @vagrant.cli("destroy", "--force")
         end
 
         def start
-          raise "Not Implemented!"
+          @vagrant.cli("up")
         end
 
         def stop
-          raise "Not Implemented!"
+          @vagrant.cli("suspend")
         end
 
         def info
-          raise "Not Implemented!"
+          if lab_exists?
+            # labs.each do |lab|
+            lab = @vagrant.primary_vm
+              @stdout.puts("Instance ID: #{lab.name}")
+              # @stdout.puts("State: #{lab.state}")
+              @stdout.puts("Username: #{lab.config.ssh.username}")
+              if lab.config.ssh.host
+                @stdout.puts
+                @stdout.puts("IP Address..: #{lab.config.ssh.host}")
+              end
+              # if (lab.dns_name || lab.private_dns_name)
+              #   @stdout.puts
+              #   @stdout.puts("DNS:")
+              #   @stdout.puts("  Public...: #{lab.dns_name}") if lab.dns_name
+              #   @stdout.puts("  Private..: #{lab.private_dns_name}") if lab.private_dns_name
+              # end
+              # if (lab.tags.count > 0)
+              #   @stdout.puts
+              #   @stdout.puts("Tags:")
+              #   lab.tags.to_hash.each do |k,v|
+              #     @stdout.puts("  #{k}: #{v}")
+              #   end
+              # end
+              if lab.config.ssh.host
+                @stdout.puts
+                @stdout.puts("Chef-Server WebUI:")
+                @stdout.puts("  http://#{lab.config.ssh.host}:4040/")
+              end
+              @stdout.puts
+            # end
+          else
+            @stdout.puts("There are no cucumber-chef test labs to display information for!")
+          end
+
+        rescue Exception => e
+          Cucumber::Chef.logger.fatal { e.message }
+          Cucumber::Chef.logger.fatal { e.backtrace.join("\n") }
+          raise VagrantError, e.message
         end
 
-        def labs_exists?
-          raise "Not Implemented!"
+        def lab_exists?
+          (@vagrant.vms.count > 0)
         end
 
         def labs
-          raise "Not Implemented!"
+          @vagrant.vms
         end
 
         def labs_running
-          raise "Not Implemented!"
+          @vagrant.vms
         end
 
         def labs_shutdown
-          raise "Not Implemented!"
+          Array.new # @vagrant.vms
         end
 
         def public_ip
-          raise "Not Implemented!"
+          @vagrant.primary_vm.config.ssh.host
         end
 
         def private_ip
-          raise "Not Implemented!"
+          @vagrant.primary_vm.config.ssh.host
+        end
+
 ################################################################################
 
         def ip
