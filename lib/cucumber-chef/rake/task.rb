3b809a91dbc1c2c99079f92fe8394386a577260c
zpatten <zpatten@jovelabs.com>
2013-01-25 18:08:07 -0800

    updated rake tasks; excluded more chef support dirs

--- lib/cucumber-chef/rake/task.rb
+++ lib/cucumber-chef/rake/task.rb
@@ -3,12 +3,22 @@ require 'cucumber'
 require 'cucumber/rake/task'
 
 Cucumber::Rake::Task.new(:"cucumber-chef") do |t|
+  feature_dirs = Array.new
+
+  feature_dir_glob = File.join(Dir.pwd, "*cookbook*", "*", "*feature*")
+  feature_dirs << Dir.glob(feature_dir_glob)
+
+  feature_dir_glob = File.join(Dir.pwd, "*feature*")
+  feature_dirs << Dir.glob(feature_dir_glob)
+
   opts = [
     "--exclude support/cookbooks",
     "--exclude support/data_bags",
+    "--exclude support/environments",
+    "--exclude support/keys",
     "--exclude support/roles",
-    "--exclude support/environments"
-  ]
+    feature_dirs
+  ].flatten.compact.join(" ")
 
-  t.cucumber_opts = opts.join(" ")
+  t.cucumber_opts = opts
 end
