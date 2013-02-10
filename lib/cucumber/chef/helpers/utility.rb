6c181bce7faf19948bd169c412bc56ddf71537d5
Zachary Patten <zpatten@jovelabs.com>
2013-02-09 16:27:23 -0800

    cosmetic refactors mostly

--- lib/cucumber/chef/helpers/utility.rb
+++ lib/cucumber/chef/helpers/utility.rb
@@ -34,29 +34,28 @@ module Cucumber::Chef::Helpers::Utility
                168..168,
                0..254,
                1..254 ]
-    ip = ""
+    ip = Array.new
     for x in 1..4 do
-      ip += octets[x-1].to_a[rand(octets[x-1].count)].to_s
-      ip += "." if x != 4
+      ip << octets[x-1].to_a[rand(octets[x-1].count)].to_s
     end
-    ip
+    ip.join(".")
   end
 
 ################################################################################
 
   def generate_mac
-    digits = [ %w( 0 ),
-               %w( 0 ),
-               %w( 0 ),
-               %w( 0 ),
-               %w( 5 ),
-               %w( e ),
-               %w( 0 1 2 3 4 5 6 7 8 9 a b c d e f ),
-               %w( 0 1 2 3 4 5 6 7 8 9 a b c d e f ),
-               %w( 5 6 7 8 9 a b c d e f ),
-               %w( 3 4 5 6 7 8 9 a b c d e f ),
-               %w( 0 1 2 3 4 5 6 7 8 9 a b c d e f ),
-               %w( 0 1 2 3 4 5 6 7 8 9 a b c d e f ) ]
+    digits = [ %w(0),
+               %w(0),
+               %w(0),
+               %w(0),
+               %w(5),
+               %w(e),
+               %w(0 1 2 3 4 5 6 7 8 9 a b c d e f),
+               %w(0 1 2 3 4 5 6 7 8 9 a b c d e f),
+               %w(5 6 7 8 9 a b c d e f),
+               %w(3 4 5 6 7 8 9 a b c d e f),
+               %w(0 1 2 3 4 5 6 7 8 9 a b c d e f),
+               %w(0 1 2 3 4 5 6 7 8 9 a b c d e f) ]
     mac = ""
     for x in 1..12 do
       mac += digits[x-1][rand(digits[x-1].count)]
