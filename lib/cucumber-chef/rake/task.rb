69421b37e4283a582a1cc36436ed8f6ac8684bd3
Zachary Patten <zpatten@jovelabs.com>
2013-02-11 20:52:43 -0800

    refactored ci rake task

--- lib/cucumber-chef/rake/task.rb
+++ lib/cucumber-chef/rake/task.rb
@@ -6,54 +6,69 @@ namespace :cc do
 
   desc "Run features in a style suitable for Continuous Integration"
   task :ci do |t|
-    pushed = false
-    exit_codes = Array.new
 
-    puts("Cleaning up...")
-    system(%Q{rm -rfv artifacts})
-    system(%Q{rm -rfv public})
-    system(%Q{mkdir -v public})
-    system(%Q{echo "<html><head><title>Cucumber-Chef Report</title></head><body>" | tee public/index.html})
-    system(%Q{echo "<h2>Cucumber-Chef Report</h2><ul>" | tee -a public/index.html})
+    def feature_dir(dir, &block)
+      puts("=" * 80)
+      puts("feature_dir(#{dir.inspect})")
+      puts("=" * 80)
 
-    puts("Generating Cucumber-Chef html reports...")
-    Dir.glob("features/*").each do |feature_dir|
-      found_features = false
-      Dir.glob("#{feature_dir}/*.feature") do |feature|
-        if found_features == false
-          found_features = true
-          system(%Q{echo "<li /><p>#{File.basename(feature_dir)}<ul>" | tee -a public/index.html})
-        end
-        filename = feature.gsub(/(\w*).feature/, '\1.html')
-        puts("#{feature} -> public/#{filename}")
-
-        system(%Q{mkdir -pv #{File.dirname("public/#{filename}")}})
-
-        push = (pushed ? nil : %Q{SETUP="YES"})
-        command = [push, "bundle exec cucumber", "features/support", feature, ENV['EXTRA_CUCUMBER_ARGS'], "--format html", "--out public/#{filename}"].flatten.compact.join(" ")
-        pushed = true if !pushed
-        puts("command=#{command.inspect}")
-        system(command)
-        exit_codes << $?.exitstatus
-
-        passed = "<font style='font-weight: bold; color: green;'>PASSED</font>"
-        failed = "<font style='font-weight: bold; color: red;'>FAILED</font>"
-        status = ((exit_codes[-1] == 0) ? passed : failed)
-
-        system(%Q{echo "<li /><a href='#{filename}'>#{File.basename(filename.gsub('.html', ''))}</a> (#{status})" | tee -a public/index.html})
-      end
-
-      if found_features
-        system(%Q{echo "</p></ul>" | tee -a public/index.html})
-      end
+      system(%Q{echo "<li /><p>#{File.basename(dir)}<ul>" | tee -a #{@index_html}})
+      block.call
+      system(%Q{echo "</p></ul>" | tee -a #{@index_html}})
     end
 
+    def run_feature(feature_file)
+      filename = feature_file.gsub(/(\w*).feature/, '\1.html')
+      puts("#{feature_file} -> public/#{filename}")
+
+      system(%Q{mkdir -pv #{File.dirname("public/#{filename}")}})
+
+      push = (@pushed ? nil : %Q{SETUP="YES"})
+      output_file = File.join(@output_dir, filename)
+      command = [push, "bundle exec cucumber", "features/support", feature_file, ENV['EXTRA_CUCUMBER_ARGS'], "--format html", "--out", output_file].flatten.compact.join(" ")
+      @pushed = true if !@pushed
+      puts("command=#{command.inspect}")
+      # system(command)
+      @exit_codes << $?.exitstatus
+
+      passed = "<font style='font-weight: bold; color: green;'>PASSED</font>"
+      failed = "<font style='font-weight: bold; color: red;'>FAILED</font>"
+      status = ((@exit_codes[-1] == 0) ? passed : failed)
+
+      system(%Q{echo "<li /><a href='#{filename}'>#{File.basename(filename.gsub('.html', ''))}</a> (#{status})" | tee -a #{@index_html}})
+    end
+
+    @output_dir = File.join(Dir.pwd, "public")
+    @index_html = File.join(@output_dir, "index.html")
+    @pushed = false
+    @exit_codes = Array.new
+
+    puts("Cleaning up...")
+    system(%Q{rm -rfv .cucumber-chef/aws/artifacts})
+    system(%Q{rm -rfv public})
+    system(%Q{mkdir -v public})
+
+    puts("Generating Cucumber-Chef html reports...")
+
+    system(%Q{echo "<html><head><title>Cucumber-Chef Report</title></head><body>" | tee public/index.html})
+    system(%Q{echo "<h2>Cucumber-Chef Report</h2><ul>" | tee -a public/index.html})
+    Dir.glob("features/*").each do |fdir|
+      if File.directory?(fdir)
+        if (ffiles = Dir.glob("#{fdir}/*.feature")).count > 0
+          feature_dir(fdir) do
+            ffiles.each do |ffile|
+              run_feature(ffile)
+            end
+          end
+        end
+      else
+        run_feature(fdir)
+      end
+    end
     system(%Q{echo "</ul></body></html>" | tee -a public/index.html})
 
-    system(%Q{/bin/bash -c '[[ -d $CUCUMBER_CHEF_HOME/aws/artifacts ]] && mv -v $CUCUMBER_CHEF_HOME/aws/artifacts .'})
-
     # If we had a failure be sure to surface it
-    exit_code = (exit_codes.any?{ |ec| ec != 0 } ? 1 : 0)
+    exit_code = (@exit_codes.any?{ |ec| ec != 0 } ? 1 : 0)
     exit(exit_code)
   end
 
