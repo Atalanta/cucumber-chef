7eb351e50884a5208a09757dff737b027b0b3416
pikesley <github@orgraphone.org>
2013-04-10 17:27:34 +0100

    Not quite sure how to handle this one

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,26 +1,13 @@
-#!/usr/bin/env ruby
-
-$: << File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
-$: << File.expand_path(File.dirname(__FILE__))
-
 require 'rspec/expectations'
 require 'cucumber/chef'
+require 'cucumber/chef/helpers'
+require 'cucumber/chef/steps'
 
 class CustomWorld
   include Cucumber::Chef
+  include Cucumber::Chef::Helpers
 end
 
 World do
   CustomWorld.new
-end
-
-Around('@invalid_credentials') do |scenario, block|
-  # Move current working directory if one exists (and restore at end)
-  FileUtils.mv(".chef", ".chef_cucumber_temp") if File.exist?(".chef")
-  FileUtils.mkdir_p(".chef")
-  config = "chef_node_name 'REALLYBOGUSORGNAME'"
-  File.open(".chef/knife.rb", 'w') { |f| f.puts config }
-  block.call
-  FileUtils.rm_rf(".chef")
-  FileUtils.mv(".chef_cucumber_temp", ".chef") if File.exist?(".chef_cucumber_temp")
 end
